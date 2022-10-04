1b48a338febe6ea9f4cd6dd2f1df0e3f9660ce48
Your Name <luszczynski.dominik@gmail.com>
2022-10-03 20:05:28 -0400

    BRAV-33 Implement unique username front+backend

--- GainzHub/backend/models/User.js
+++ GainzHub/backend/models/User.js
@@ -16,6 +16,10 @@ const UserSchema = new mongoose.Schema({
     password:{
         type: String,
         unique: true
+    },
+    username:{
+        type: String,
+        unique: true
     }
 });
 
