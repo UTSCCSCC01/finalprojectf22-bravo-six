bf15f1f518ff26f1761548fc48f375f335fc19e5
Bryan-Wan <84968862+Bryan-Wan@users.noreply.github.com>
2022-10-05 23:58:46 -0400

    BRAV-36 NavBar now functional. Routes to 4 pages.

---
+++ GainzHub/frontend/screens/Nutrition.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+
+export default function Nutrition({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text
+                onPress={() => alert('This is the "Nutrition" screen.')}
+                style={{ fontSize: 26, fontWeight: 'bold' }}>Nutrition</Text>
+        </View>
+    );
+}
--- GainzHub/frontend/screens/Social.js
+++ GainzHub/frontend/screens/Social.js
@@ -6,12 +6,12 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 const Tab = createBottomTabNavigator();
 
 
-export default function Home({ navigation }) {
+export default function Social({ navigation }) {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text
-                onPress={() => alert('This is the "Home" screen.')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>Home Screen</Text>
+                onPress={() => alert('This is the "Social" screen.')}
+                style={{ fontSize: 26, fontWeight: 'bold' }}>Social</Text>
         </View>
     );
 }
