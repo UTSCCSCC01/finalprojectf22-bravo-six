d9255628f370050314ddf616ae5b3dc0ec9821da
Aviraj Waraich <a.waraich@mail.utoronto.ca>
2022-10-07 23:41:23 -0400

    BRAV-39 Backend for adding workouts/muscle groups to the DB

--- GainzHub/backend/routes/authRoutes.js
+++ GainzHub/backend/routes/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const {registerUser, checkRegister, checkLogin, loginUser} = require('../controllers/authController');
+const {registerUser, checkRegister, checkLogin, loginUser, CreateWorkout} = require('../controllers/authController');
 
 router.post("/", checkRegister, registerUser);
 router.post("/login", checkLogin, loginUser);
+
+router.post("/CreateWorkouts", CreateWorkout);
 
 module.exports = (router);
