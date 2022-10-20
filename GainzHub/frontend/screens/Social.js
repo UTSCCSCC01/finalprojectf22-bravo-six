df563d05070814629ad8c8efe30e2f9fbd312ac2
dopplerz <ryan_ramroop@outlook.com>
2022-10-20 16:48:46 -0400

    Fixed InputFields to match Figma

--- GainzHub/frontend/screens/Social.js
+++ GainzHub/frontend/screens/Social.js
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import {TextInput} from "react-native-paper";
+import { Colors } from '../components/colors';
 const Tab = createBottomTabNavigator();
 
 
@@ -12,6 +13,19 @@ export default function Social({ navigation }) {
             <Text
                 onPress={() => alert('This is the "Social" screen.')}
                 style={{ fontSize: 26, fontWeight: 'bold' }}>Social</Text>
+        <TextInput 
+        style={styles.textInputLine}
+        activeUnderlineColor={Colors.maroon} 
+        mode = "flat" label = "Height" 
+        placeholder = "Enter Height"
+        />
+
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    textInputLine:{
+        backgroundColor: Colors.appBackground,
+    }
+})
