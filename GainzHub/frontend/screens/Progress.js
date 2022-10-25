37edcd3a5b78d6ccbcd2d4a23a7894c025cb286f
Bryan-Wan <84968862+Bryan-Wan@users.noreply.github.com>
2022-10-25 01:32:11 -0400

    BRAV-71 Created Progress Page and Added Progress Tab to NavBar

---
+++ GainzHub/frontend/screens/Progress.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+
+export default function Nutrition({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text
+                onPress={() => alert('This is the "Progress" screen.')}
+                style={{ fontSize: 26, fontWeight: 'bold' }}>Progress</Text>
+        </View>
+    );
+}
