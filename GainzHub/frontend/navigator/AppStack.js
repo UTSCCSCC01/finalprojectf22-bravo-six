1b3f71654985901cf31597cb47843f3bd4710a80
dom_ <luszczynski.dominik@gmail.com>
2022-10-26 19:27:07 -0400

    BRAV-63 Create pages + figured out sending data

--- GainzHub/frontend/navigator/AppStack.js
+++ GainzHub/frontend/navigator/AppStack.js
@@ -18,6 +18,7 @@ import Workout from '../screens/Workout';
 import WorkoutAddPlan from '../screens/WorkoutAddPlan';
 import WorkoutExplore from '../screens/WorkoutExplore';
 import WorkoutLogs from '../screens/WorkoutLogs';
+import NutritionMealPlanEditor from '../screens/NutritionMealPlanEditor';
 
 const Stack = createStackNavigator();
 
@@ -42,6 +43,7 @@ const AppStack = ()=>{
                 <Stack.Screen name = "WorkoutAddPlan" component={WorkoutAddPlan}/>
                 <Stack.Screen name = "WorkoutExplore" component={WorkoutExplore}/>
                 <Stack.Screen name = "WorkoutLogs" component={WorkoutLogs}/>
+                <Stack.Screen name = "NutritionMealPlanEditor" component={NutritionMealPlanEditor}/>
             </Stack.Navigator>
         </NavigationContainer>
     );
